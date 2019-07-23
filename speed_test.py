0ecf8a9cfb1ba92b03536e6a922db46e1da45553
Kohei Adachi <d13301@yonago.kosen-ac.jp>
2019-07-23 11:03:06 +0900

    update

--- speed_test.py
+++ speed_test.py
@@ -4,6 +4,7 @@ from httplib2 import Http
 import gspread
 import datetime
 
+
 def speedtest_results():
     servers = []
     # If you want to test against a specific server
@@ -24,46 +25,51 @@ def speedtest_results():
 
     return results_dict
 
+
 def get_network_status_data():
     results_dict = speedtest_results()
-    upload,download,ping = 0,0,0
-    upload=round(results_dict["download"]/(10**6),2)
-    download=round(results_dict["upload"]/(10**6),2)
-    ping = round(results_dict["ping"],2)
+    upload, download, ping = 0, 0, 0
+    upload = round(results_dict["download"] / (10 ** 6), 2)
+    download = round(results_dict["upload"] / (10 ** 6), 2)
+    ping = round(results_dict["ping"], 2)
     time = results_dict["timestamp"]
     # print(upload)
     # print(download)
     # print(ping)
-    column_list = [download,upload,ping]
+    column_list = [download, upload, ping]
     return column_list
+
 
 def load_spread_sheet():
     scopes = ['https://www.googleapis.com/auth/spreadsheets']
-    json_file = 'raspi-56a886c9c4f1.json'#OAuth用クライアントIDの作成でダウンロードしたjsonファイル
+    json_file = 'raspi-56a886c9c4f1.json'  # OAuth用クライアントIDの作成でダウンロードしたjsonファイル
     credentials = ServiceAccountCredentials.from_json_keyfile_name(json_file, scopes=scopes)
     http_auth = credentials.authorize(Http())
 
     # スプレッドシート用クライアントの準備
-    doc_id = '1n6MuYkKRViTPGUJQX9V4GIFXZo5j1EvQkYXNqNL3nFM'#これはスプレッドシートのURLのうちhttps://docs.google.com/spreadsheets/d/以下の部分です
+    doc_id = '1n6MuYkKRViTPGUJQX9V4GIFXZo5j1EvQkYXNqNL3nFM'  # これはスプレッドシートのURLのうちhttps://docs.google.com/spreadsheets/d/以下の部分です
     client = gspread.authorize(credentials)
-    gfile   = client.open_by_key(doc_id)#読み書きするgoogle spreadsheet
+    gfile = client.open_by_key(doc_id)  # 読み書きするgoogle spreadsheet
     return gfile
+
 
 def add_new_sheet(date_today):
     gfile = load_spread_sheet()
     worksheet = gfile.add_worksheet(title=date_today, rows="300", cols="20")
     # write columns
-    worksheet.update_acell('A1','time')
+    worksheet.update_acell('A1', 'time')
     worksheet.update_acell('B1', 'download')
-    worksheet.update_acell('C1','upload')
-    worksheet.update_acell('D1','ping')
+    worksheet.update_acell('C1', 'upload')
+    worksheet.update_acell('D1', 'ping')
 
     return worksheet
+
 
 def load_sheet(date_today):
     gfile = load_spread_sheet()
     worksheet = gfile.worksheet(date_today)
     return worksheet
+
 
 # 日付のWorksheetがなけらば作成する．あればロードする
 def create_or_load_worksheet(date_today):
@@ -76,15 +82,17 @@ def create_or_load_worksheet(date_today):
 
     return worksheet
 
+
 def upload_network_status_data():
     date_today = str(datetime.date.today())
     worksheet = create_or_load_worksheet(date_today)
     now_time = datetime.datetime.now().strftime("%H:%M:%S")
 
     column_list = get_network_status_data()
-    column_list.insert(0,now_time)
+    column_list.insert(0, now_time)
     # column_list = [11,22,33,44]
     worksheet.append_row(column_list)
+
 
 if __name__ == "__main__":
     # selecting_data()
